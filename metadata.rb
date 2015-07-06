994be99dafd071776270c6ba545094bc611f70d8
Christian Fischer <sw-dev@computerlyrik.de>
2015-07-06 12:24:41 +0200

    added source url to attributes

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,6 @@ maintainer       "computerlyrik"
 maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures dspam"
+source_url "https://github.com/computerlyrik/chef-dspam"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.2"
