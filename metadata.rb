65d5f2c37ed6c8b1a3164a20baec21e5edf94ba9
Christian Fischer <sw-dev@computerlyrik.de>
2015-07-06 13:01:56 +0200

    update version to devel cycle

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          "Apache 2.0"
 description      "Installs/Configures dspam"
 source_url "https://github.com/computerlyrik/chef-dspam"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          "0.1.4-Snapshot"
 
 depends 'apt'
